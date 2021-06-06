fe90dcdc6d7cc3030e88efadf6b2393efb435d2a
Madoxen <pchlad5@gmail.com>
2021-06-06 17:18:00 +0200

    basic testing

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ x = stroke.iloc[:, :-1]
 y = stroke.iloc[:, -1]
 
 x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.20, random_state=1)
+    x, y, test_size=0.30, random_state=1)
 
 gender = ctrl.Antecedent(x_train["gender"], 'gender')
 age = ctrl.Antecedent(x_train["age"], 'age')
@@ -91,41 +91,56 @@ bmi.automf()
 smoking_status.automf()
 stroke.automf()
 
-rule1 = ctrl.Rule(heart_disease['good'] & avg_glucose_level['good'], stroke['good'])
-rule2 = ctrl.Rule(heart_disease['good'] & avg_glucose_level['mediocre'], stroke['good'])
+rule1 = ctrl.Rule(heart_disease['good'] &
+                  avg_glucose_level['good'], stroke['good'])
+rule2 = ctrl.Rule(heart_disease['good'] &
+                  avg_glucose_level['mediocre'], stroke['good'])
 rule3 = ctrl.Rule(heart_disease['good'], stroke['good'])
 rule4 = ctrl.Rule(smoking_status['good'], stroke['good'])
 rule5 = ctrl.Rule(smoking_status['poor'], stroke['poor'])
 rule6 = ctrl.Rule(heart_disease['poor'], stroke['poor'])
-rule7 = ctrl.Rule(heart_disease['poor'] & smoking_status['poor'], stroke['poor'])
-rule8 = ctrl.Rule(heart_disease['good'] & smoking_status['good'], stroke['good'])
-rule9 = ctrl.Rule(heart_disease['good'] & smoking_status['mediocre'], stroke['good'])
+rule7 = ctrl.Rule(heart_disease['poor'] &
+                  smoking_status['poor'], stroke['poor'])
+rule8 = ctrl.Rule(heart_disease['good'] &
+                  smoking_status['good'], stroke['good'])
+rule9 = ctrl.Rule(heart_disease['good'] &
+                  smoking_status['mediocre'], stroke['good'])
 rule10 = ctrl.Rule(heart_disease['good'], stroke['good'])
 rule11 = ctrl.Rule(bmi['good'], stroke['good'])
 rule12 = ctrl.Rule(bmi['poor'], stroke['poor'])
 rule13 = ctrl.Rule(heart_disease['poor'], stroke['poor'])
 rule14 = ctrl.Rule(bmi['poor'] & age['poor'], stroke['poor'])
-rule15 = ctrl.Rule(heart_disease['good'] & avg_glucose_level['good'], stroke['good'])
-rule16 = ctrl.Rule(heart_disease['good'] & avg_glucose_level['mediocre'], stroke['good'])
+rule15 = ctrl.Rule(heart_disease['good'] &
+                   avg_glucose_level['good'], stroke['good'])
+rule16 = ctrl.Rule(heart_disease['good'] &
+                   avg_glucose_level['mediocre'], stroke['good'])
 rule17 = ctrl.Rule(heart_disease['good'], stroke['good'])
 rule18 = ctrl.Rule(work_type['good'], stroke['good'])
 rule19 = ctrl.Rule(work_type['poor'], stroke['poor'])
 rule20 = ctrl.Rule(Residence_type['poor'], stroke['poor'])
 rule21 = ctrl.Rule(age['poor'] & work_type['poor'], stroke['poor'])
-rule22 = ctrl.Rule(heart_disease['good'] & avg_glucose_level['good'], stroke['good'])
+rule22 = ctrl.Rule(heart_disease['good'] &
+                   avg_glucose_level['good'], stroke['good'])
 rule23 = ctrl.Rule(age['good'] & avg_glucose_level['good'], stroke['good'])
 rule24 = ctrl.Rule(heart_disease['good'], stroke['good'])
 rule25 = ctrl.Rule(age['good'], stroke['good'])
 rule26 = ctrl.Rule(age['poor'], stroke['poor'])
 rule27 = ctrl.Rule(hypertension['poor'], stroke['poor'])
 rule28 = ctrl.Rule(gender['poor'] & ever_married['poor'], stroke['poor'])
-rule29 = ctrl.Rule(heart_disease['good'] & avg_glucose_level['good'], stroke['good'])
-rule30 = ctrl.Rule(heart_disease['good'] & avg_glucose_level['mediocre'], stroke['good'])
-rule30 = ctrl.Rule(heart_disease['good'] & avg_glucose_level['mediocre'], stroke['good'])
-rule20 = ctrl.Rule(Residence_type['good'] | bmi['average'], stroke['poor'])
-rule20 = ctrl.Rule(gender['good'] | age['good'] | hypertension['poor'] | heart_disease['poor'] | ever_married['good'] | work_type['good'] | Residence_type['good'] | bmi['average'], stroke['poor'])
+rule29 = ctrl.Rule(heart_disease['good'] &
+                   avg_glucose_level['good'], stroke['good'])
+rule30 = ctrl.Rule(heart_disease['good'] &
+                   avg_glucose_level['mediocre'], stroke['good'])
+rule30 = ctrl.Rule(heart_disease['good'] &
+                   avg_glucose_level['mediocre'], stroke['good'])
+rule31 = ctrl.Rule(Residence_type['good'] | bmi['average'], stroke['poor'])
+rule32 = ctrl.Rule(gender['poor'] | age['poor'] | hypertension['poor'] | heart_disease['poor'] |
+                   ever_married['poor'] | work_type['poor'] | Residence_type['poor'] | bmi['poor'], stroke['poor'])
+rule33 = ctrl.Rule(gender['good'] | age['good'] | hypertension['good'] | heart_disease['good'] |
+                   ever_married['good'] | work_type['good'] | Residence_type['good'] | bmi['good'], stroke['good'])
 
-stroke_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5,rule6,rule7,rule8,rule9,rule10,rule11,rule12,rule13,rule14,rule15,rule16, rule17, rule18, rule19, rule20,rule21,rule22,rule23,rule24,rule25,rule26,rule27,rule28,rule29,rule30])
+stroke_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9, rule10, rule11, rule12, rule13, rule14,
+                                  rule15, rule16, rule17, rule18, rule19, rule20, rule21, rule22, rule23, rule24, rule25, rule26, rule27, rule28, rule29, rule30, rule31, rule32, rule33])
 stroke = ctrl.ControlSystemSimulation(stroke_ctrl)
 
 
@@ -138,15 +153,44 @@ stroke.input['work_type'] = 4.0
 stroke.input['gender'] = 1.0
 stroke.input['Residence_type'] = 2.0
 stroke.input['hypertension'] = 2.0
-stroke.input['ever_married']= 1.0
+stroke.input['ever_married'] = 1.0
 
+
+TP = 0
+TN = 0
+FP = 0
+FN = 0
+# acceptance treshold, values bigger than this treshold are positive, rest are negative
+treshold = 0.5
+
+print(y_test.value_counts())
 
 for index, row in x_test.iterrows():
     d = row.to_dict()
     stroke.inputs(d)
+    val = 0
+    true_val = y_test[index]
     stroke.compute()
-    print(stroke.output['stroke'])
+  ##  print(str(stroke.output['stroke'] > treshold) + " " + str(true_val))
+    if stroke.output['stroke'] > treshold:
+        val = 1
 
+    if val == 1:
+        if val == true_val:
+            TP += 1 #predicted 
+        else:
+            FP += 1
+    else:
+        if val == true_val:
+            TN += 1
+        else:
+            FN += 1
+
+
+print("TP " + str(TP))
+print("TN " + str(TN))
+print("FP " + str(FP))
+print("FN " + str(FN))
 
 # testing phase
 # go through the test set
